498dabd7d53a102e9c01ebc9f3aa23066f78d11d
Callista Stefanie Taswin <callistastefanie@gmail.com>
2023-03-07 10:54:12 -0800

    :mag: added google analytic tags + small transition fixes

--- src/components/IntroLayer.vue
+++ src/components/IntroLayer.vue
@@ -11,7 +11,7 @@
         <Transition
           appear
           name="intro__emoji"
-          :style="{ transitionDelay: '3s' }"
+          :style="{ transitionDelay: '2.5s' }"
         >
           <img
             src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Waving%20Hand.png"
